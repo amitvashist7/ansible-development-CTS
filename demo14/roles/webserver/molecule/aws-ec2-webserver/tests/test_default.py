8f7136c8f6eb3a3b294f7374f1225c1d57dbfda5
root <root@jenkins>
2019-01-16 21:00:17 +0000

    Apache Molecule - Docker & AWS

---
+++ demo14/roles/webserver/molecule/aws-ec2-webserver/tests/test_default.py
@@ -0,0 +1,36 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('pkg', [
+  'httpd'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('svc', [
+  'httpd'
+])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
+
+
+@pytest.mark.parametrize('file, content', [
+  ("/var/www/html/vashist/index.html", "Welcome to the world of Automation"),
+  ("/var/www/html/vashist/info.html", "Welcome to host")
+])
+def test_files(host, file, content):
+    file = host.file(file)
+
+    assert file.exists
+    assert file.contains(content)
